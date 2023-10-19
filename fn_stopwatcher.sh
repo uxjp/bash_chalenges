3112784fba95720e47c9e4b8bbbbb7dbd96d5502
JoaoPedro <joaopedro1172@gmail.com>
2023-10-18 21:33:35 -0300

    some progress

--- fn_stopwatcher.sh
+++ fn_stopwatcher.sh
@@ -1,11 +1,22 @@
 #!bin/bash
 
 function stopwatcher() {
+  INIT_TIME=$(date +'%Y-%m-%d %H:%M:%S.%N')
   $1
+  POST_TIME=$(date +'%Y-%m-%d %H:%M:%S.%N')
+
+  INIT_EPOCH=$(date -d "$INIT_TIME" +%s%3N)
+  POST_EPOCH=$(date -d "$POST_TIME" +%s%3N)
+
+  TIME_DIFF=$((POST_EPOCH - INIT_EPOCH))
+
+  echo "Time Difference: ${TIME_DIFF} milliseconds"
 }
 
-function my_fn() {
-  echo 'Hello world 2'
+function sleep5() {
+  echo "começando a dormir por 5 segundos..."
+  sleep 5
+  echo "acordei"
 }
 
-stopwatcher my_fn
+stopwatcher sleep5
